19ac3e1b627614877b3fa4c6b0429ff604f4169a
vindya325 <81144971+vindya325@users.noreply.github.com>
2021-03-22 15:48:52 +0530

    Add files via upload
    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,12 @@
+#Add implementation
+def add(x,y):
+	pass
+#Subtract implementation
+def substract(x,y):
+	pass
+#Multiply implementation
+def multiply(x,y):
+	pass
+#Divide implementation
+def divide(x,y):
+	pass
